fd2a5741518b68ffc6271e9e468046bc753aceb1
Carol <cfpitari@gmail.com>
2021-09-07 20:14:44 -0300

    second heroku

---
+++ app.js
@@ -0,0 +1,19 @@
+let express = require ("express");
+let server = express();
+let path = require ("path");
+
+server.use(express.static("public"));
+
+server.listen(3030,()=>{
+    console.log("http://localhost:3030")
+});
+
+server.get ("/",(req, res) => {
+    res.sendFile (path.resolve("./views/home.html"));
+});
+server.get ("/login",(req, res) => {
+    res.sendFile (path.resolve("./views/login.html"));
+});
+server.get ("/register",(req, res) => {
+    res.sendFile (path.resolve("./views/register.html"));
+});
